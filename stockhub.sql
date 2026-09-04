d50c85ba83ed4306f3fd4690a3720d93d4f3e30e
muhammad akbar eszu <muhammadakbareszu@gmail.com>
2026-09-04 14:11:04 +0700

    Add files via upload

---
+++ stockhub.sql
@@ -0,0 +1,177 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 04, 2026 at 09:10 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `stockhub`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `kode` varchar(100) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `kategori` varchar(100) DEFAULT NULL,
+  `stok` int(11) DEFAULT 0,
+  `satuan` varchar(50) DEFAULT NULL,
+  `minimumStok` int(11) DEFAULT 0,
+  `supplier` varchar(150) DEFAULT NULL,
+  `expiredDate` date DEFAULT NULL,
+  `rak` varchar(50) DEFAULT NULL,
+  `catatan` text DEFAULT NULL,
+  `hargaBeli` decimal(12,2) DEFAULT 0.00,
+  `hargaJual` decimal(12,2) DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode`, `nama`, `kategori`, `stok`, `satuan`, `minimumStok`, `supplier`, `expiredDate`, `rak`, `catatan`, `hargaBeli`, `hargaJual`) VALUES
+(1, 'BRG-101', 'Kecap', 'Makanan', 30, 'botol', 10, 'PT. KECAP UTAMA', '2026-09-20', 'A-01', '', 3000.00, 5000.00),
+(2, 'BRG-204', 'pringles', 'Makanan', 15, 'pcs', 10, 'PT. MAJU JAYA', '2026-10-04', 'B-02', '', 3000.00, 5000.00),
+(3, 'BRG-204', 'Kecap', 'dapur', 10, 'botol', 10, 'PT. KECAP UTAMA', '2026-09-30', 'C-13', '', 3000.00, 5000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id`, `nama`) VALUES
+(1, 'uji'),
+(2, 'makanan'),
+(3, 'minuman'),
+(4, 'dapur'),
+(5, 'skincare'),
+(6, 'obat-obatan'),
+(7, 'frozen food');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `kontak` varchar(150) DEFAULT NULL,
+  `telepon` varchar(50) DEFAULT NULL,
+  `email` varchar(150) DEFAULT NULL,
+  `lokasi` varchar(150) DEFAULT NULL,
+  `leadTime` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `nama` varchar(150) NOT NULL,
+  `role` enum('admin','bos') NOT NULL,
+  `password_hash` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `nama`, `role`, `password_hash`) VALUES
+(1, 'admin', 'Administrator', 'admin', '$2b$12$jZsSzfOB5IAEDhPk1xKfne2qeSk0yJ9mRpLHjBIDXOnsRqjs90USS'),
+(2, 'bos', 'Bos', 'bos', '$2b$12$kPPuNX2sQX7MbQYATGERXe0l6iDIeV8HZnLUENKjDBPgF1nWcWu1q');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `supplier`
+--
+ALTER TABLE `supplier`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
